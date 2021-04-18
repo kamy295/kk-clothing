a8703ee7b5d91212f4e632cfe01c96bc4ff7af4f
Kamlesh Kamble <kamlesh.kamble2.95@gmail.com>
2021-04-18 14:34:31 +0530

    Adding the hats page and routing to it using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
+
     </div>
   );
 }
